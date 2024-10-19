67254c39e4f9ef9dcfe4146eb588add2c6bf49fb
Daniagg <daniagg1506@gmail.com>
2024-10-19 13:24:29 +0700

    Update bshub.lua

--- bshub.lua
+++ bshub.lua
@@ -1,1 +1,10 @@
-
+local game = game
+local placeId = game:GetService("Players").LocalPlayer.PlaceId
+  if placeId = 17586052747 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Daniagg/bshub/refs/heads/main/17586052747.lua'))()
+  elseif placeId = 12501200809 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Daniagg/bshub/refs/heads/main/12501200809.lua'))()
+  else
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Daniagg/bshub/refs/heads/main/ifnotsuported.lua'))()
+end
+    
